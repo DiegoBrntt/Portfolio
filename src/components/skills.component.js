78bf5ea2e4701c871f2e280929ac965915f682a9
Diego Brunetti <diego.brntt12@gmail.com>
2023-02-27 17:12:52 -0300

    Habilidades cuaddro

--- src/components/skills.component.js
+++ src/components/skills.component.js
@@ -10,15 +10,18 @@ class Skills extends Component {
         <div className="table-responsive">
           <table className="table table-bordered b-2 text-center">
             <thead>
-              <tr>
+              <tr className="bg-dark text-white">
                 <th className="w-33">
                   <i className="bi bi-database fw-bold"></i>
+                  <div>Backend</div>
                 </th>
                 <th className="w-33">
-                  <i className="bi bi-database fw-bold"></i>
+                  <i className="bi bi-laptop fw-bold"></i>
+                  <div>Frontend</div>
                 </th>
                 <th className="w-33">
-                  <i className="bi bi-database fw-bold"></i>
+                  <i className="bi bi-pc-display fw-bold"></i>
+                  <div>More</div>
                 </th>
               </tr>
             </thead>
@@ -27,38 +30,89 @@ class Skills extends Component {
                 <td className="w-33">
                   <ul className="list-group list-group-flush">
                     <li className="list-group-item">
-                      <div className="fw-bold"> - JAVA (7-11) </div>
-                      <div>SpringMVC - Spring Boot - Hibernate</div>
+                      <div className="fw-bold text-decoration-underline"> JAVA (7-11) </div>
+                      <div>SpringMVC - Spring Boot - Hibernate - Servlets</div>
+                      <div className="col border bg-success text-white">Advanced</div>
                     </li>
                     <li className="list-group-item">
-                      <div className="fw-bold"> - Clojure </div>
+                      <div className="fw-bold text-decoration-underline"> C# </div>
+                      <div>.NET - ASP.NET - Dapper - Entity FrameWork</div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> Clojure </div>
                       <div>Tadam</div>
+                      <div className="col border bg-success text-white">Advanced</div>
                     </li>
                     <li className="list-group-item">
-                      <div className="fw-bold"> - Javascript </div>
-                      <div>Node, Express, Mongo, MySQL, Sequelize</div>
+                      <div className="fw-bold text-decoration-underline"> Javascript </div>
+                      <div>Node - Express - Mongo - MySQL - Sequelize</div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> C </div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> C++ </div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> SQL </div>
+                      <div>MySQL - SQLite - SQL Server</div>
+                      <div className="col border bg-warning">Intermediate</div>
                     </li>
                   </ul>
                 </td>
                 <td className="w-33">
-                <ul className="list-group list-group-flush">
+                  <ul className="list-group list-group-flush">
                     <li className="list-group-item">
-                      <div className="fw-bold"> - JAVA (7-11) </div>
-                      <div>FrameWorks: SpringMVC - Spring Boot - Hibernate</div>
+                      <div className="fw-bold text-decoration-underline"> JAVA (7-11) </div>
+                      <div>JPA - Thymeleaf</div>
+                      <div className="col border bg-warning">Intermediate</div>
                     </li>
                     <li className="list-group-item">
-                      - 
+                      <div className="fw-bold text-decoration-underline"> Javascript </div>
+                      <div>React</div>
+                      <div className="col border bg-warning">Intermediate</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> CSS </div>
+                      <div>Bootstrap</div>
+                      <div className="col border bg-warning">Intermediate</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> C# (HTMLCS) </div>
+                      <div>Razor</div>
+                      <div className="col border bg-warning">Intermediate</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> HTML </div>
+                      <div className="col border bg-warning">Intermediate</div>
                     </li>
                   </ul>
                 </td>
                 <td className="w-33">
-                <ul className="list-group list-group-flush">
+                  <ul className="list-group list-group-flush">
                     <li className="list-group-item">
-                      <div className="fw-bold"> - JAVA (7-11) </div>
-                      <div>FrameWorks: SpringMVC - Spring Boot - Hibernate</div>
+                      <div className="fw-bold text-decoration-underline"> Programming Paradigms </div>
+                      <div>Structured -  Object Oriented - Functional</div>
+                      <div className="col border bg-success text-white">Advanced</div>
                     </li>
                     <li className="list-group-item">
-                      - 
+                      <div className="fw-bold text-decoration-underline"> Diagrams </div>
+                      <div>UML</div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> Data Structure </div>
+                      <div>Linked List (simple - double) - Circle Linked List (simple - double) - Stack - Queue - Trees</div>
+                      <div className="col border bg-success text-white">Advanced</div>
+                    </li>
+                    <li className="list-group-item">
+                      <div className="fw-bold text-decoration-underline"> Video game development </div>
+                      <div>Unity3D</div>
+                      <div className="col border bg-warning">Intermediate</div>
                     </li>
                   </ul>
                 </td>
