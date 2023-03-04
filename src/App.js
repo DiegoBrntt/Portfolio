270833fe3da0cd16cd6d60e690c0e124132b48ca
Diego Brunetti <diego.brntt12@gmail.com>
2023-03-04 18:46:49 -0300

    Nuevas Skills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavHome from "./components/navBar.component";
 
@@ -12,11 +11,9 @@ class App extends Component {
   render() {
     return (
       <div className="w-100">
-        <Router>
           <NavHome />
           <br />
           <Skills />
-        </Router>
       </div>
     );
   }
