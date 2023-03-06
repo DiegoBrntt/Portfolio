36df1fd142ec61c909a70924f94ec1ed6f92f930
Diego Brunetti <diego.brntt12@gmail.com>
2023-03-05 23:33:07 -0300

    grid and proyectos

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 import NavHome from "./components/navBar.component";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Skills from "./components/skills.component";
 import Welcome from "./components/welcome.component";
 import Grid from "./components/grid.component";
-
+import Proyecto from "./components/proyectos.component";
 
 
 class App extends Component {
@@ -15,10 +16,11 @@ class App extends Component {
       <div className="w-100">
           <NavHome />
           <Welcome />
+          <Proyecto />
+          <Skills />
           <br/>
           <Grid />
           <br/>
-          <Skills />
       </div>
     );
   }
--- src/components/grid.component.js
+++ src/components/grid.component.js
@@ -4,6 +4,8 @@ import idea from "../components/imagenes/solucion.png";
 import lenguajes from "../components/imagenes/lenguajes.png";
 import back from "../components/imagenes/back.png";
 import gaming from "../components/imagenes/gaming.png";
+import patrones from "../components/imagenes/patrones.png";
+import full from "../components/imagenes/full.png";
 
 class Grid extends Component {
   render() {
@@ -28,18 +30,21 @@ class Grid extends Component {
             <div className="col">
             <div className="card border-0">
                 <div className="text-center">
-                <img src={lenguajes} className="card-img-top" alt="..." />
+                <img src={patrones} className="card-img-top" alt="..." />
                 </div>
                 
                 <div className="card-body">
                   <p className="card-text">
-                    Un conocimiento avanzado de los paradigmas de la programación me facilitan la adaptación 
-                    a nuevas tecnologias que van surgiendo a través del tiempo, permitiendome el perfeccionarme cada dia un poco más.
+                    Buen manejo en diferentes patrones de diseños como Facade,
+                    Strategy, MVC, MVVM, Factory. Aplicados junto con principios SOLID y buenas practicas
+                    de codificación.
                   </p>
                 </div>
               </div>
             </div>
-            <div className="col">
+          </div>
+          <div className="row text-center">
+          <div className="col">
             <div className="card border-0">
                 <div className="text-center">
                 <img src={back} className="card-img-top" alt="..." />
@@ -53,17 +58,32 @@ class Grid extends Component {
                 </div>
               </div>
             </div>
+            <div className="col">
+              <div className="card border-0">
+                <div className="text-center">
+                <img src={full} className="card-img-top" alt="gaming" />
+                </div>
+                
+                <div className="card-body">
+                  <p className="card-text">
+                    Manejo de stacks y frameworks para desarrollo fullstack. 
+                    Siendo MERN (Mongo, Express, React, Node) y MySQL, Sequelize, Express, React, Node de los cuales 
+                    tengo experiencia. De facil adaptaciónn a nuevos stacks.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="row text-center">
             <div className="col">
               <div className="card border-0">
                 <div className="text-center">
-                <img src={gaming} className="card-img-top" alt="..." />
+                <img src={gaming} className="card-img-top" alt="gaming" />
                 </div>
                 
                 <div className="card-body">
                   <p className="card-text">
-                    Experiencia en el desarrollo y prohramación de videojuegos con Unity3D (C#).
+                    Experiencia en el desarrollo y programación de videojuegos con Unity3D (C#).
                     En generos de tower defense y space shooter.
                   </p>
                 </div>
@@ -77,26 +97,13 @@ class Grid extends Component {
                 
                 <div className="card-body">
                   <p className="card-text">
-                    Un conocimiento avanzado de los paradigmas de la programación me facilitan la adaptación 
+                    Un conocimiento avanzado de los paradigmas de la programación (estructurada, orientaa a objetos y funcional) me facilitan la adaptación 
                     a nuevas tecnologias que van surgiendo a través del tiempo, permitiendome el perfeccionarme cada dia un poco más.
                   </p>
                 </div>
               </div>
             </div>
-            <div className="col">
-            <div className="card border-0">
-                <div className="text-center">
-                <img src={back} className="card-img-top" alt="..." />
-                </div>
-                
-                <div className="card-body">
-                  <p className="card-text">
-                    Mayormente orientado al area de Backend, con conocimientos basicos de 
-                    creación de Base de datos y normalización. Tanto en SQL y NoSQL. 
-                  </p>
-                </div>
-              </div>
-            </div>
+            
           </div>
         </div>
       </>
